7cad3063faa47d8afb42283b790dd941f8c76c8a
Kim-EunSu <kes73640@gmail.com>
2022-12-27 21:56:43 +0900

    이미지&json추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Top = styled.div``;
+const Location = styled.div``;
+const Temp = styled.div``;
+const Description = styled.div``;
+const Feels = styled.p``;
+const Bottom = styled.p``;
+const Humidity = styled.p``;
+const Wind = styled.p``;
 
 function App() {
+  //const url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=3bbbcbe27f124138733419d7708043c8";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Top>
+        <Location>
+          <p>Dollars</p>
+        </Location>
+        <Temp>
+          <h1>60℉</h1>
+        </Temp>
+        <Description>
+          <Feels>Clouds</Feels>
+          <Bottom>60℉</Bottom>
+          <Humidity>20%</Humidity>
+          <Wind>12 MPH</Wind>
+        </Description>
+      </Top>
+    </Container>
   );
 }
 
